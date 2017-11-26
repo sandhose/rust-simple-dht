4bb1a75c0515c97509f900b5fa05977678d07893
Quentin Gliech <quentin.gliech@etu.unistra.fr>
2017-11-26 19:45:07 +0100

    Echo back the messages

--- src/server.rs
+++ src/server.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 use std::io;
 use std::sync::Arc;
 use std::time::Duration;
-use futures::{Stream, Future, IntoFuture};
+use futures::{Sink, Stream, Future};
 use tokio_core::net::{UdpFramed, UdpSocket};
 use tokio_core::reactor::Handle;
 use tokio_timer::Timer;
@@ -25,29 +25,29 @@ impl Server {
     }
 
     pub fn run(self) -> Box<Future<Item = (), Error = io::Error>> {
-        let (_, stream) = self.socket.split();
+        let (sink, stream) = self.socket.split();
 
         let timer = Timer::default();
         let interval = timer.interval(Duration::from_secs(1));
         let timer_state = self.state.clone();
-        let timer_future = interval.for_each(move |_| {
+        let timer_stream = interval.filter_map(move |_| {
             println!("Tick. {:?}", timer_state);
             timer_state.remove_stale();
-            Ok(())
+            None
         });
-        let timer_future = timer_future.map_err(|e| io::Error::from(e));
+        let timer_stream = timer_stream.map_err(|e| io::Error::from(e));
 
         let server_state = self.state.clone();
-        let server_future = stream.for_each(move |(src, msg)| {
+        let server_stream = stream.filter_map(move |(src, msg)| {
             println!("Got message from {}: {:?}", src, msg);
             server_state.probe(src);
-            Ok(())
+            Some((src, msg))
         });
 
-        let combined_future = (server_future, timer_future)
-            .into_future()
-            .map(|_| ());
+        let combined_stream = server_stream.select(timer_stream);
 
-        Box::new(combined_future)
+        let future = sink.send_all(combined_stream).map(|_| ());
+
+        Box::new(future)
     }
 }
